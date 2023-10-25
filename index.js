90a5a8bad900661716cf2beb45792d4f1566cf22
Sajithrajan03 <saisajith03@gmail.com>
2023-10-25 23:33:16 +0530

    auth

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ import axios from 'axios'
 app.use(cors())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
-app.get('/auth',(req,res)=>{
+app.get('/',(req,res)=>{
     const oauth2client = new google.auth.OAuth2(
         // 'client_id',
         // 'client_secret',
@@ -24,7 +24,7 @@ app.get('/auth',(req,res)=>{
     )
     
     const scopes = ["https://www.googleapis.com/auth/fitness.activity.read profile email openid"]
-    const urls = oauth2client.generateAuthUrl({
+    const url = oauth2client.generateAuthUrl({
         access_type:"offline",
         scope:scopes,
         state:JSON.stringify({
@@ -34,12 +34,12 @@ app.get('/auth',(req,res)=>{
         
     })
     
-    request(urls,(err,response,body)=>{
+    request(url,(err,response,body)=>{
         console.log("error:",err)
         console.log("statusCode:",response && response.statusCode)
-        res.send(urls)
+        res.send(url)
     })
-    // res.send(`<script>window.open("${urls}");</script>`);
+    // res.redirect('url');
 })
 
 
@@ -52,7 +52,7 @@ app.get('/steps',async (req,res)=>{
         // 'redirect_uris'
         "510019787897-9hfjvuho1cnsf88j150cec2q5l38ghn8.apps.googleusercontent.com",
         "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",
-        "http://localhost:1234/nutrition"
+        "http://localhost:1234/steps"
     )
     const token = await oauth2client.getToken(code)
     
@@ -89,7 +89,7 @@ app.get('/steps',async (req,res)=>{
                 
                 for (const point of dataSet.dataset){
                     for (const steps of point.point){
-                        console.log(steps.value)
+                        console.log(steps.value[0].intVal)
                     }
                     
                 }
@@ -102,64 +102,10 @@ app.get('/steps',async (req,res)=>{
 )
 
 app.get('/nutrition',async (req,res)=>{
-    const queryUrl = new urlParse(req.url)
-     const code = queryParse.parse(queryUrl.query).code
-     const oauth2client = new google.auth.OAuth2(
-        // 'client_id',
-        // 'client_secret',
-        // 'redirect_uris'
-        "510019787897-9hfjvuho1cnsf88j150cec2q5l38ghn8.apps.googleusercontent.com",
-        "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",
-        "http://localhost:1234/nutrition"
-    )
-    const token = await oauth2client.getToken(code)
-    
-    
 
-    let stepArray = []
-    try{
-        const result =  await axios({
-            method:"POST",
-            headers:{
-                Authorization:"Bearer "+token.tokens.access_token
-            },
-            "content-Type":"application/json",
-            url:"https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate",
-        data:{
-            "aggregateBy": [{
-                "dataSourceId":
-                  "raw:com.google.nutrition:407408718192:MyDataSource"
-              }],
-              "bucketByTime": { "durationMillis": 86400000 },
-              "startTimeMillis": 1697135400000,
-              "endTimeMillis": 1697653800000 
-        }
-        })
-        
-        stepArray =  result.data
-        console.log(stepArray)
-        }  
-        catch(error){
-            console.log(error)  
-        }
+    // console.log(queryUrl ,code, oauth2client)
 
-        try {
-            for (const dataSet of stepArray){
-                
-                // for (const point of dataSet.dataset){
-                //     for (const steps of point.point){
-                //         console.log(steps.value)
-                //     }
-                    
-                // }
-                // console.log(dataSet)
-            }
-        } catch (error) {
-            
-        }
-       
-    }
-)
+})
 app.listen(port,()=>{
-    console.log(`Listening at http://localhost:${port}`)
+    console.log(`Listening at http://localhost/${port}`)
 })
